bbcb75b456980f4c920470820d73dc780bd88d46
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-01 08:43:37 +0530

    Update 1752.SortedRotatedArray.cpp

--- Arrays/1752.SortedRotatedArray.cpp
+++ Arrays/1752.SortedRotatedArray.cpp
@@ -4,3 +4,39 @@
 */
 
 Code:
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        
+        int count=0;
+        int n=nums.size();
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>nums[(i+1)%n])count++;
+        }
+        
+        return count<=1;
+    }
+};
+
+/*class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 1; i<n; i++){
+            if(nums[i-1] > nums[i]){
+                count++;
+            }
+         }
+        if(nums[n-1] > nums[0]){
+            count++;
+            
+            return cout <= 1;
+        }
+         
+    }
+};
+*/
