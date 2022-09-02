e110acccf8746366b1f72ffa531974ca00fac269
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-02 23:41:08 +0530

    Create 268.MissingNumbers.cpp

---
+++ Arrays/268.MissingNumbers.cpp
@@ -0,0 +1,17 @@
+// Missiing numbers problem 
+code
+//Intuitive Approach
+//Time Complexity: O(N)
+//Space Complexity : O(1)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) { // find the missing number in a array
+        int sum = 0; // sum of all numbers initially 0
+        int total = nums.size()*(nums.size() + 1)/2; // total is the sum of all numbers in the array
+        for (auto number : nums) { // iterate through the array
+            sum += number; // add the number to the sum
+        }
+        return total - sum; // return the difference between the sum and the total that is the missing number
+    }
+};
