1b7c98f120de72f757046a6c7c52cb7fd1838505
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-04 02:57:47 +0530

    Update 2395.Find_SubarraysEqualSum.cpp

--- Arrays/2395.Find_SubarraysEqualSum.cpp
+++ Arrays/2395.Find_SubarraysEqualSum.cpp
@@ -53,3 +53,18 @@ public:
         return false;   
     }    
 };
+
+//2nd approch 
+class Solution {
+public:
+    bool findSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i=1;i<n;i++){
+            int sum = nums[i]+nums[i-1];
+            mp[sum]++;
+            if(mp[sum]>1) return true;
+        }
+        return false;
+    }
+};
