f4eb023d8d02da8bdd503fd1d906cf997ea21a64
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-14 10:55:30 +0530

    Update 81.Search_Sorted_rotated_array_II.cpp

--- Arrays/81.Search_Sorted_rotated_array_II.cpp
+++ Arrays/81.Search_Sorted_rotated_array_II.cpp
@@ -43,3 +43,46 @@ public:
     }
 	
 };
+
+
+//
+Code: 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    int s = 0;
+    int e = nums.size()-1; 
+    int mid;
+        
+    while(s <= e){
+        
+            mid= s + (e - s)/2;
+            if(nums[mid]==target)
+                return true;
+            if((nums[s] == nums[mid]) && (nums[e] == nums[mid]))
+            {
+                s++;
+                e--;
+            }
+            else if(nums[s]<=nums[mid])
+            {
+                if(target<nums[mid] && target>=nums[s])
+                    e=mid-1;
+                else
+                    s=mid+1;
+            }
+            
+            else
+            {
+                if(target>nums[mid] && target<=nums[e])
+                   s=mid+1;
+                else
+                    e=mid-1;
+            }
+            
+        }
+        return false;
+    }
+};
+
+
