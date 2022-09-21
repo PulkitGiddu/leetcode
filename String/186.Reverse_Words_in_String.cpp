a452ef3a225477a3e0d0c727e7b69368dd75175e
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-21 22:40:48 +0530

    Create 186.Reverse_Words_in_String.cpp

---
+++ String/186.Reverse_Words_in_String.cpp
@@ -0,0 +1,15 @@
+186. Reverse words in a stirng 
+/*
+Q --> 
+Given a character array s , reverse the order of words 
+A word  is defined as a sequence of non-space character. The words in s will be seprated 
+by a single space 
+your code must solve the problem in place ie: without allocate extra space 
+*/
+
+/*
+if we found the space then reverse the word
+\0 Null character reverse the words.. 
+ try out question :
+*/
+code: 
