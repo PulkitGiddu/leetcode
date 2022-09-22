2146ff7597fba0427b5385338d8bed5557370bb3
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-22 22:56:55 +0530

    Create 1910.Remove_all_occurence_of_string.cpp

---
+++ String/1910.Remove_all_occurence_of_string.cpp
@@ -0,0 +1,23 @@
+//1910. Remove all occurence form of a substring 
+/*
+Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+Find the leftmost occurrence of the substring part and remove it from s.
+Return s after removing all occurrences of part.
+A substring is a contiguous sequence of characters in a string.
+
+Example 1:
+
+Input: s = "daabcbaabcbc", part = "abc"
+Output: "dab"
+*/
+code:
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length() != 0 && s.find(part) < s.length() ){
+            s.erase(s.find(part), part.length());
+        }
+        return s; 
+    }
+};
+
