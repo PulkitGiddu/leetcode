a30fa8ecabbab1f3ec5eb6a46afdd1c435286ff1
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-26 21:32:31 +0530

    Update 5.Longest_palidrome_substring.cpp

--- String/5.Longest_palidrome_substring.cpp
+++ String/5.Longest_palidrome_substring.cpp
@@ -11,8 +11,9 @@ Then we make left = i - 1, while s[left] == s[right], s[left, right] inclusive i
 Finally we update the tracked longest palindrome if needed.
 */
 class Solution{
-public:
+    public:
 string longestPalindrome(string s) {
+    
     if (s.empty()) return "";
     
     if (s.size() == 1) return s;
@@ -20,14 +21,19 @@ string longestPalindrome(string s) {
     int min_start = 0, max_len = 1;
     
     for (int i = 0; i < s.size();) {
-    if (s.size() - i <= max_len / 2) break;
-    
+        if (s.size() - i <= max_len / 2) break;
         int j = i, k = i;
-        while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
+        
+        while (k < s.size()-1 && s[k+1] == s[k]) 
+        ++k; // Skip duplicate characters.
         i = k+1;
-        while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+        
+        while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) 
+        { ++k; --j; } // Expand.
+        
         int new_len = k - j + 1;
-        if (new_len > max_len) { min_start = j; max_len = new_len; }
+        if (new_len > max_len) 
+        { min_start = j; max_len = new_len; }
     }
     return s.substr(min_start, max_len);
 }
