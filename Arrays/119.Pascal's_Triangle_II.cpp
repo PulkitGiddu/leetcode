f101f3504660833e054ecd6ad455dab53c3230a1
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-09-30 20:42:47 +0530

    Update 119.Pascal's_Triangle_II.cpp

--- Arrays/119.Pascal's_Triangle_II.cpp
+++ Arrays/119.Pascal's_Triangle_II.cpp
@@ -35,3 +35,18 @@ public:
         return currentRow;
     }
 };
+
+Code: 
+class Solution {
+public:
+    vector<int> getRow(int r) {
+        vector<int>v(r+1);
+        long temp=1;
+        v[0]=v[r]=1;
+        for(int i=1,up=r,down=1;i<r;i++,up--,down++){
+            temp = temp*up/down;
+            v[i]=temp;
+        }
+        return v;
+    }
+};
