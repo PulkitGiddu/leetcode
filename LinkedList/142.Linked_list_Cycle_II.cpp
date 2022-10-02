e1814967d5c0bd5e8b0d83da093fb38821188dc1
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-10-02 21:40:16 +0530

    Update 142.Linked_list_Cycle_II.cpp

--- LinkedList/142.Linked_list_Cycle_II.cpp
+++ LinkedList/142.Linked_list_Cycle_II.cpp
@@ -23,26 +23,18 @@ Explanation: There is no cycle in the linked list.
  
 */
 code:
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL)
+        if(head == NULL || head->next == NULL) // if there is empty list or only one node is there return NULL;
             return NULL;
         ListNode* slow = head;
         ListNode* fast = head;
         ListNode* entry = head;
         
         while(fast->next && fast->next->next){
-            slow = slow->next;
-            fast = fast->next->next;
+            slow = slow->next;             // Moving slow by one position 
+            fast = fast->next->next;       // Fast by 2 positions 
             if(slow == fast){              // there is a cycle 
                 while(slow != entry){      // found the entry location.
                 slow = slow->next;
