caf766b7da923b2c2c78ab5378ea6c168a871fb8
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-10-12 13:06:09 +0530

    Update 234.Palindrome_Linked_List.cpp

--- LinkedList/234.Palindrome_Linked_List.cpp
+++ LinkedList/234.Palindrome_Linked_List.cpp
@@ -45,4 +45,60 @@ public:
 };
 
 //T.C = O(n)
-//S.C = O(1)
+//S.C = O(1
+
+class Solution{
+private: 
+    Node* getMid(Node* head){
+        Node* slow = head; 
+        Node* fast = head -> next;
+        while(fast != NULL && fast -> next != NULL ){
+          
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        return slow;
+    }
+    
+    Node* reverse(Node* head){
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+        while(curr != NULL){
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next; 
+        }
+        return prev;
+    }
+    
+public:
+    bool isPalindrome(Node* head) {
+        if(head == NULL || head -> next == NULL)
+            return true;
+        // step 1: Find MID
+        Node* middle = getMid(head);
+        // step 2: reverse the list after the middle element we have 
+        Node* temp = middle -> next; 
+        middle -> next = reverse(temp);
+        // step 3: Compare both halves
+        Node* head1 = head;
+        Node* head2 = middle -> next;
+        while(head2 != NULL){
+            if(head1 -> data != head2 -> data){
+                return false;
+            }
+            head1= head1 -> next;
+            head2 = head2 -> next;
+        }
+        // Step 4: repeat step 2
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        return true;
+    }
+};
+
+// T.C = O(n)
+// S.C = O(1)
