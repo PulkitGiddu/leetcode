e4a7022e102c369e157b3373bc3a05962e63a90a
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-10-15 14:41:05 +0530

    Update 430.Flatten_a_Multilevel_Doubly_Linked_List.cpp

--- LinkedList/430.Flatten_a_Multilevel_Doubly_Linked_List.cpp
+++ LinkedList/430.Flatten_a_Multilevel_Doubly_Linked_List.cpp
@@ -27,3 +27,15 @@ public:
         return head;
     }
 };
+
+Code2: 
+class Solution{
+    public:
+Node* flatten(Node* head, Node* rest = nullptr) {
+  if (!head) return rest;
+  head->next = flatten(head->child, flatten(head->next, rest));
+  if (head->next) head->next->prev = head;
+  head->child = nullptr;
+  return head;
+}
+};
