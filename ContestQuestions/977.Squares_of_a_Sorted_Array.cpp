c398dc6b7fc7ca3944a3dc832b86615bb67ddc93
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-10-20 01:13:12 +0530

    Update 977.Squares_of_a_Sorted_Array.cpp

--- ContestQuestions/977.Squares_of_a_Sorted_Array.cpp
+++ ContestQuestions/977.Squares_of_a_Sorted_Array.cpp
@@ -69,3 +69,32 @@ public:
             return result ;
     }
 };
+
+
+Code2: using two pointer approch: 
+
+class Solution{
+public: 
+vector<int> sortedSquares(vector<int>& nums) {
+
+    vector<int> res(nums.size()); 
+
+    int l = 0, r = nums.size()-1; 
+
+    for(int i = nums.size()-1; i>=0; i--){
+    
+        if(abs(nums[l]) > nums[r] ){
+        res[i] = nums[l]*nums[l];
+        l++;
+        }
+    
+        else{
+        res[i] = nums[r]*nums[r];
+        r--;
+    }
+}
+return res;
+    }
+}; 
+
+
