16f99a15b62b94df8f71629ca4b68475b8a5b6ab
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-10-25 18:48:54 +0530

    Update 22.Generate_Parentheses.cpp

--- Recursion/22.Generate_Parentheses.cpp
+++ Recursion/22.Generate_Parentheses.cpp
@@ -3,8 +3,8 @@ Code:
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        addingpar(res, "", n, 0);
+        vector<string> res;   // created new resultant string where parenthesis will be stored
+        addingpar(res, "", n, 0);          
         return res;
     }
     void addingpar(vector<string> &v, string str, int n, int m){
@@ -12,7 +12,7 @@ public:
             v.push_back(str);
             return;
         }
-        if(m > 0){ addingpar(v, str+")", n, m-1); }
-        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+        if(m > 0){ addingpar(v, str+")", n, m-1); }  // apply close bracket and moving forward. 
+        if(n > 0){ addingpar(v, str+"(", n-1, m+1); } // apply open bracket and moving forward. 
     }
 };
