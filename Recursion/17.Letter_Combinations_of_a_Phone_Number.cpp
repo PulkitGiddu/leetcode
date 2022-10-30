8aa5335fcfb34a557670bbcea186b036822c5153
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-10-30 23:10:23 +0530

    Update 17.Letter_Combinations_of_a_Phone_Number.cpp

--- Recursion/17.Letter_Combinations_of_a_Phone_Number.cpp
+++ Recursion/17.Letter_Combinations_of_a_Phone_Number.cpp
@@ -107,3 +107,38 @@ public:
         return result;
     }
 };
+
+
+Code3: using queue data structues 
+class Solution {
+public:
+	//Method - 3 Queue
+    vector<string> letterCombinations(string digits) {
+    
+        vector<string> phone={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+    if(digits.empty())
+        return {};
+    
+        queue<string> q;
+        vector<string> ans;
+        
+        q.push("");
+        
+        while(!q.empty())
+        {
+            string curr = q.front();
+            q.pop();
+            
+            if(curr.length()==digits.length())
+                ans.push_back(curr);
+            else{
+                string s = phone[digits[curr.length()] - '0'];
+                for(auto x: s)
+                    q.push(curr + x);
+            }
+            
+        }
+        return ans;
+    }
+};
