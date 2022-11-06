5913e037acec1edef99ed417655db8167ae5d6b3
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-11-06 22:22:03 +0530

    Update i.The_Celebrity_Problem.cpp

--- Stack/i.The_Celebrity_Problem.cpp
+++ Stack/i.The_Celebrity_Problem.cpp
@@ -80,3 +80,78 @@ class Solution
     
     }
 };
+
+Code: 
+//better explaniantion
+// O(N)approach using stacks.
+
+class Solution 
+{
+    private:
+    bool knows(vector<vector<int> >& M,int a, int b){
+        if(M[a][b]==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public:
+
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+//first we push the people 0,1,2...etc on to the stack.
+
+    {
+      stack<int> s;
+      for(int i=0;i<n;i++){
+          s.push(i);
+        }
+//now we compare the the top two values of the stack for whether they know each other or not, knows function is written above.
+//if A knows B then A is not a celebrity and B is re added on to the  staack....this process is continued until only a single element present on stack.
+
+        while(s.size()>1){
+        int a = s.top();
+        s.pop();
+        int b = s.top();
+        s.pop();
+        if(knows(M,a,b)){
+            s.push(b);
+        }
+        else{
+            s.push(a);
+        }
+     }
+
+        //verify thhe value present at the stack for whether its a celebrity or not, as it could be or not a celebrity, 
+        //just check rows and columns for whether it knows everyone or not, also whether everyone knows them or not.
+
+        int potential=s.top();
+        int colcount=0;
+        int rowcount=0;
+        
+        for(int i=0;i<n;i++){
+            if(M[i][potential]==1){
+                colcount++;
+            }
+            if(M[potential][i]==0){
+                rowcount++;
+            }
+            if(colcount==n-1&&rowcount==n){
+                return potential;
+            }
+        }
+
+        return -1;
+
+//we return the answer depending on the result, all of these use N comparisons with no nested loops, hence its 0(N) TC....
+
+        
+
+        
+
+    
+
+    }
+
+};
