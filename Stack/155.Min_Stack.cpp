3c439b21b4925d860e391b11d96f9950e8343455
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-11-10 22:05:17 +0530

    Create 155.Min_Stack.cpp

---
+++ Stack/155.Min_Stack.cpp
@@ -0,0 +1,34 @@
+Q - 155. Min Stack. 
+  
+Code: 
+class MinStack {
+public:
+        
+    stack<int> st, s2;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(s2.empty() || val <= s2.top()){
+                s2.push(val);
+        }
+            st.push(val); 
+    }
+    
+    void pop() {
+        if(st.top() == s2.top()){
+              s2.pop();  
+        }
+            st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+       return s2.top(); 
+    }
+};
+
