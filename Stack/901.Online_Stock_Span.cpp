260709e34481865381d4f9c985873910d9730683
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-11-23 22:48:20 +0530

    Create 901.Online_Stock_Span.cpp

---
+++ Stack/901.Online_Stock_Span.cpp
@@ -0,0 +1,37 @@
+Q - 901. Online Stock Span 
+
+Code: 
+class StockSpanner {
+public:
+    StockSpanner() {
+        // maintain a monotonic stack for stock entry
+        
+	// definition of stock entry:
+        // first parameter is price quote
+        // second parameter is price span
+    }
+    
+    int next(int price) {
+        
+        int curPrice = price;
+        int curSpan = 1;
+        
+        // Compute price span in stock data with monotonic stack
+        while( stack.size() && stack.back().first <= price ){
+            
+            auto [prevPrice, prevSpan] = stack.back();
+            
+            stack.pop_back();
+            
+            // update current price span with history data in stack
+            curSpan += prevSpan;
+        }
+        
+        // update latest price quote and price span
+        stack.push_back( pair<int, int>{curPrice, curSpan} );
+        
+        return curSpan;
+    }
+private:
+    vector< pair<int, int> > stack;
+};
