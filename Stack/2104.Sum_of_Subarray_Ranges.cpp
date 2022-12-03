8bc2312aa39fb3211303939ac1c9603f03c4d84b
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-12-03 18:35:26 +0530

    Create 2104.Sum_of_Subarray_Ranges.cpp

---
+++ Stack/2104.Sum_of_Subarray_Ranges.cpp
@@ -0,0 +1,20 @@
+Q - 2104. Sum of Subarray Ranges 
+
+Code: 
+class Solution{
+public:
+long long subArrayRanges(vector<int>& A) {
+        long long res = 0;
+        for (int i = 0; i < A.size(); i++) {
+            int ma = A[i], mi = A[i]; // two variable declaring maximum and minimum 
+            for (int j = i; j < A.size(); j++) {
+                    
+                ma = max(ma, A[j]);
+                mi = min(mi, A[j]);
+                    
+                res += ma - mi; // the difference between max and min value.
+            }
+        }
+        return res;
+    }
+};
