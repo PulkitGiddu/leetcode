0fe05dae78f57a2407e06745a0a90c228c2ae996
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2022-12-07 08:54:08 +0530

    Create 746.Min_Cost_Climbing_Stairs.cpp

---
+++ Stack/746.Min_Cost_Climbing_Stairs.cpp
@@ -0,0 +1,14 @@
+Q - 746. Min Cost Climbing Stairs. 
+
+Code : 
+class Solution {
+public:
+
+ int minCostClimbingStairs(vector<int>& cost) {
+        for(int i=2;i<cost.size();i++)
+        {
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[cost.size()-1],cost[cost.size()-2]);
+    }
+};
