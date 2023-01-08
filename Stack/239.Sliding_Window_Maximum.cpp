ef44d0b70a47f9dfdbefbf70c8713d6ec8b700cf
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-01-08 23:10:14 +0530

    Update 239.Sliding_Window_Maximum.cpp

--- Stack/239.Sliding_Window_Maximum.cpp
+++ Stack/239.Sliding_Window_Maximum.cpp
@@ -7,12 +7,13 @@ class Solution{
         deque<int> dq;
         vector<int> ans;
         for(int i = 0; i<nums.size(); i++){
-            if(!dq.empty() && dq.front() == i - k) dq.pop_front();
+            if(!dq.empty() && dq.front() == i - k) dq.pop_front();   // check for out of bond cases
 
-            while(!dq.empty() && nums[dq.back()] <= nums[i])
+            while(!dq.empty() && nums[dq.back()] <= nums[i])  // remove smaller elements then a[i]
             dq.pop_back();
 
-            dq.push_back(i);
+            dq.push_back(i);  // after that we will store the current index
+            
             if(i>=k-1) 
             ans.push_back(nums[dq.front()]);
         }
