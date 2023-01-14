69a387758025ace1ff6d6ffd7da22f0db6d3ac5e
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-01-14 15:49:17 +0530

    Create 94.Binary_Tree_Inorder_Traversal.cpp

---
+++ Tree/94.Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,21 @@
+Q - 94. Binary Tree Inorder Traversal. 
+  Code: 
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        stack<TreeNode*> todo;
+        while (root || !todo.empty()) {
+            while (root) {
+                todo.push(root);
+                root = root -> left;
+            }
+            root = todo.top();
+            todo.pop();
+            nodes.push_back(root -> val);
+            root = root -> right;
+        }
+        return nodes;
+    }
+};
