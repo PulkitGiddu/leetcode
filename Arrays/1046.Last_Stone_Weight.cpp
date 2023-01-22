97cd088e3f46238aad68925b3c89c42c0bb0e93f
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-01-22 22:49:53 +0530

    Create 1046.Last_Stone_Weight.cpp

---
+++ Arrays/1046.Last_Stone_Weight.cpp
@@ -0,0 +1,19 @@
+1046. Last Stone Weight  ----------- Nvidia asked question 
+
+Code: 
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(begin(stones), end(stones)); 
+        while(pq.size() > 1){
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            if(x>y)
+            pq.push(x-y);
+
+        }
+        return pq.empty() ?0 : pq.top();
+        
+    }
+};
