c56bffd5f85726cf7216fd9242bcbe2f1a0f1c21
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-01-27 21:19:06 +0530

    Update 472.Concatenated_Words.cpp

--- ContestQuestions/472.Concatenated_Words.cpp
+++ ContestQuestions/472.Concatenated_Words.cpp
@@ -29,4 +29,37 @@ public:
     }
 };
 
-// Time complexity: O(n^2*m) //where n is the number of words in the input array and m is the average length of the words.
+// Time complexity: O(n^2*m) where n is the number of words in the input array and m is the average length of the words.
+
+
+Code: Solution using dp. 
+    
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> words_set;
+        for (string word : words) words_set.insert(word);
+        vector<string> res;
+    
+        for (string word : words) {
+            int n = word.size();
+            vector<int> dp(n + 1, 0);
+            dp[0] = 1;
+            for (int i = 0; i < n; i++) {
+                if (!dp[i]) continue;
+                for (int j = i + 1; j <= n; j++) {
+                    if (j - i < n && words_set.count(word.substr(i, j - i))) {
+                        dp[j] = 1;
+                    }
+                }
+                if (dp[n]) {
+                    res.push_back(word);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+//Time complexity: The time complexity of this solution is O(n * L^2), where n is the number of words in the input list and L is the maximum length of the words.
