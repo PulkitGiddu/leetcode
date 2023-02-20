dc38e29165bf6c9e88ce2c06b92be97521d54a69
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-02-20 21:29:23 +0530

    Create 35.Search_Insert_Position.cpp

---
+++ DailyLeetcode_Feb_2023/35.Search_Insert_Position.cpp
@@ -0,0 +1,21 @@
+35. Search Insert Position 
+
+Code:
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1; 
+        while(low <= high){
+
+            int mid = low + (high - low)/2; 
+
+            if(target == nums[mid]) return mid; 
+
+            else if(target > nums[mid]) low = mid + 1;
+            
+            else high = mid - 1;
+        }         
+        return low;
+    }
+};
