eafb55e02f36ff2da36becc4c6b8961cd6153455
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-03-24 00:20:14 +0530

    Update 1319. Number of Operations to Make Network Connected.cpp

--- DailyLeetcode_March_2023/1319. Number of Operations to Make Network Connected.cpp
+++ DailyLeetcode_March_2023/1319. Number of Operations to Make Network Connected.cpp
@@ -1,9 +1,7 @@
-1319. Number of Operations to Make Network Connected 
-
-Code: 
 class Solution {
 public:
     void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src){
+        // normal dfs 
         visited[src] = true;
         for(int i : adj[src])
             if(!visited[i])
@@ -11,20 +9,26 @@ public:
     }
 
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size() < n - 1) return -1;
+        if(connections.size() < n - 1) return -1; // if no of wires is less than n-1 then its not possible.
+        
+        // making an graph 
         vector<vector<int>> adj(n);
         for(auto c : connections){
             adj[c[0]].push_back(c[1]);
             adj[c[1]].push_back(c[0]);
         }
 
-        vector<bool> visited(n, false);
+        vector<bool> visited(n, false); // keeping visited to only visit an node once. 
+        
+        // Counting total no of connected components by dfs on every non visited element 
         int components = 0;
         for(int i=0; i<n; i++)
             if(!visited[i]){
                 dfs(adj, visited, i);
-                components++;
+
+                components++; // In one dfs visit all nodes in that component so ++ one component added 
             }
-        return components - 1;
+
+        return components - 1; 
     }
 };
