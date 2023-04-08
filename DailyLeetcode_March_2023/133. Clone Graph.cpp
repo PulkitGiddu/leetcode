c3d7f2dace69222fc58e95a7e7b8b0f907154f8c
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-04-08 12:48:40 +0530

    Create 133. Clone Graph.cpp

---
+++ DailyLeetcode_March_2023/133. Clone Graph.cpp
@@ -0,0 +1,39 @@
+133. Clone Graph 
+
+Code: 
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+public:
+    unordered_map<Node*, Node*> copies;
+
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        if (copies.find(node) == copies.end()) {
+            copies[node] = new Node(node->val);
+            for (Node* neighbor : node->neighbors) {
+                copies[node]->neighbors.push_back(cloneGraph(neighbor));
+            }
+        }
+        return copies[node];
+    }
+};
