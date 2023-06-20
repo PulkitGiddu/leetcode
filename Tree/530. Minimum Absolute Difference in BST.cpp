94091e7205076cfad667a55b06e8610c50371d38
Pulkit Giddu <101356458+PulkitGiddu@users.noreply.github.com>
2023-06-20 14:49:38 +0530

    Create 530. Minimum Absolute Difference in BST.cpp

---
+++ Tree/530. Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,25 @@
+530. Minimum Absolute Difference in BST
+// using inorder traversal 
+
+
+class Solution {
+public:
+    TreeNode* prev = NULL; 
+    void solve(TreeNode* root, int& ans){
+        if(root -> left){
+            solve(root -> left, ans); 
+        }
+        if(prev!= NULL){
+            ans = min(ans , abs(prev -> val - root -> val));
+        }
+        prev = root;
+        if(root -> right){
+            solve(root -> right, ans);
+        }
+    }
+    int getMinimumDifference(TreeNode* root) {
+      int ans = INT_MAX; 
+      solve(root , ans); 
+      return ans; 
+    }
+};
